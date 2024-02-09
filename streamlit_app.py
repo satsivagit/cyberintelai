87a54dd06d8d2bf413d0eb21bbbee02f97e4daaf
satsivagit <159522819+satsivagit@users.noreply.github.com>
2024-02-09 14:24:09 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+# Set the title of the app
+st.title('My Streamlit App')
+
+# Display some text on the front-end
+st.write('Welcome Cyber Threat Intelligence')
+
+# Create a text input field
+user_input = st.text_input("Enter some text:")
+
+# Display the user input back to the front-end
+st.write("You entered:", user_input)
+
+# Create a slider
+slider_value = st.slider('Select a number:', 0, 100, 50)
+
+# Display the slider value on the front-end
+st.write("You selected:", slider_value)
+
+# Create a checkbox
+if st.checkbox('Show/hide some text'):
+    st.write('This is some hidden text that appears when the checkbox is checked.')
